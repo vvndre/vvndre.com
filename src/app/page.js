113162ed2e46f85c92805eb0deb425a59d0ac80a
andrercastillon <andrercastillon@gmail.com>
2024-02-23 15:47:01 -0600

    added and removed images from public folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import NavBar from "../components/navbar/NavBar";
-
 export default function Home() {
-  return <main className="flex min-h-screen flex-col bg-dark-void"></main>;
+  return <main className="flex min-h-screen flex-col bg-dark-void">home</main>;
 }
