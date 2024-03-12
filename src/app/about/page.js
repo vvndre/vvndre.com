ddad75ced663d698337366f115c286f9c85df9d5
andrercastillon <andrercastillon@gmail.com>
2024-03-12 17:33:25 -0500

    header formatting for about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,12 @@
 export default function About() {
   return (
-    <div className="flex min-h-screen flex-col bg-dark-void">about</div>
+    <div className="flex min-h-screen flex-col bg-dark-void">
+      <div className="py-8 container-xl text-center">
+        <p className="text-dusty-gray mb-6">About</p>
+        <h2 className="text-6xl mb-8">
+          👋 Hey I'm, <br /> Andre Castillon.
+        </h2>
+      </div>
+    </div>
   );
 }
