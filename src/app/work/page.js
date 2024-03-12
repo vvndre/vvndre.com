4077f8fe1816fa7ee026524392551bba78ef897d
andrercastillon <andrercastillon@gmail.com>
2024-03-12 17:19:24 -0500

    header formatting for work page

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -1,7 +1,12 @@
 export default function Work() {
-  return ( 
-  <div className="flex min-h-screen flex-col bg-dark-void">
-    Work
-  </div> 
+  return (
+    <div className="flex min-h-screen flex-col px-8 bg-dark-void">
+      <div className="py-8 container-xl text-center">
+        <p className="text-dusty-gray mb-6">Work</p>
+        <h2 className="text-6xl mb-8">
+          A small selection of <br /> recent projects.
+        </h2>
+      </div>
+    </div>
   );
 }
