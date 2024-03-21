8636f1a3faf054924e0731347eb674c6407a9579
andrercastillon <andrercastillon@gmail.com>
2024-03-21 17:59:01 -0500

    created RotatingText Component for home page and edited css

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import portrait from "/public/images/Portrait-BW.png";
+import RotatingText from "@/components/rotating-text/RotatingText";
 
 export default function Home() {
-  const name = "Andre Castillon".split("");
-
   return (
-    <main className="flex min-h-screen px-6 items-center flex-col bg-dark-void">
-      <div className="flex justify-center items-center relative mt-20">
+    <main className="flex min-h-screen mx-8 px-6 items-center flex-col bg-dark-void">
+      <div className="py-8 container-xl text-center"></div>
+      <div className="relative">
         <Image
           src={portrait}
           width={600}
           height={600}
           alt="Picture of the author"
           quality={100}
-          className="relative rounded-full z-1"
+          className="rounded-full"
         />
+        <RotatingText />
       </div>
     </main>
   );
---
+++ src/components/rotating-text/RotatingText.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./rotating-text.css";
+
+const RotatingText = () => {
+  return (
+    <div className="rounded-text rotating absolute inset-0 flex justify-center items-center">
+      <svg viewBox="0 0 200 200">
+        <path
+          id="textPath"
+          d="M 85,0 A 85,85 0 0 1 -85,0 A 85,85 0 0 1 85,0"
+          transform="translate(100,100)"
+          fill="none"
+        ></path>
+        <g fontSize="18.5px">
+          <text textAnchor="start">
+            <textPath href="#textPath" startOffset="0%">
+              - Andre Castillon - Web Developer - UX/UI Designer
+            </textPath>
+          </text>
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default RotatingText;
