ea9e275e870d16646d3fb3a189fbf2ce3132de95
andrercastillon <andrercastillon@gmail.com>
2024-03-22 12:38:04 -0500

    (refactor)homepage & RotatingText component

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ import RotatingText from "@/components/rotating-text/RotatingText";
 export default function Home() {
   return (
     <main className="flex min-h-screen mx-8 px-6 items-center flex-col bg-dark-void">
-      <div className="py-8 container-xl text-center"></div>
+      <div className="py-12 container-xl text-center"></div>
       <div className="relative">
         <Image
           src={portrait}
-          width={600}
-          height={600}
+          width={700}
+          height={700}
           alt="Picture of the author"
           quality={100}
           className="rounded-full"
