9c10bf7b5695a9c33f3512546254ddfdb0163ebb
andrercastillon <andrercastillon@gmail.com>
2024-04-07 15:06:24 -0500

    (feat): Implement automatic slide transition & colored RxDot corresponding to the img

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,15 +1,14 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 import Image from "next/image";
-import image1 from "/public/images/IMG_2824.JPG";
+import image3 from "/public/images/IMG_2824.JPG";
 import image2 from "/public/images/IMG_3742.jpg";
-import image3 from "/public/images/IMG_3445.JPG";
+import image1 from "/public/images/IMG_3445.JPG";
 
 export default function Carousel() {
   const slides = [image1, image2, image3];
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
@@ -27,6 +26,15 @@ export default function Carousel() {
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      nextSlide();
+    }, 4200);
+
+    return () => clearInterval(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentIndex]);
+
   return (
     <div className="max-w-[1200px] h-[68vh] w-full pb-12 px-4 relative group z-10">
       <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500 relative">
@@ -39,11 +47,11 @@ export default function Carousel() {
         />
       </div>
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-snow-white cursor-cell">
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-snow-white hover:text-liquid-lava cursor-cell">
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
       {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-snow-white cursor-cell">
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-snow-white hover:text-liquid-lava cursor-cell">
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
       <div className="flex top-4 justify-center py-2">
@@ -51,7 +59,11 @@ export default function Carousel() {
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className="text-2xl"
+            className={`text-2xl hover:cursor-cell ${
+              slideIndex === currentIndex
+                ? "text-snow-white"
+                : "text-dusty-gray"
+            }`}
           >
             <RxDotFilled />
           </div>
