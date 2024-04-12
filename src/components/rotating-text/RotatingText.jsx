23e709a7c72d82868dc4b39288e760b9593ba961
andrercastillon <andrercastillon@gmail.com>
2024-04-12 17:05:50 -0500

    (refactor): removed head tag and added bnack use client

--- src/components/rotating-text/RotatingText.jsx
+++ src/components/rotating-text/RotatingText.jsx
@@ -1,31 +1,25 @@
-// "use client";
+"use client";
 import "./rotating-text.css";
-import Head from "next/head";
 
 const RotatingText = () => {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <div className="rounded-text rotating absolute inset-0 flex justify-center items-center">
-        <svg viewBox="0 0 200 200">
-          <path
-            id="textPath"
-            d="M 85,0 A 85,85 0 0 1 -85,0 A 85,85 0 0 1 85,0"
-            transform="translate(100,100)"
-            fill="none"
-          ></path>
-          <g fontSize="1.156rem">
-            <text textAnchor="start">
-              <textPath href="#textPath" startOffset="0%">
-                - Andre Castillon - Web Developer - UX / UI Designer
-              </textPath>
-            </text>
-          </g>
-        </svg>
-      </div>
-    </>
+    <div className="rounded-text rotating absolute inset-0 flex justify-center items-center">
+      <svg viewBox="0 0 200 200">
+        <path
+          id="textPath"
+          d="M 85,0 A 85,85 0 0 1 -85,0 A 85,85 0 0 1 85,0"
+          transform="translate(100,100)"
+          fill="none"
+        ></path>
+        <g fontSize="1.156rem">
+          <text textAnchor="start">
+            <textPath href="#textPath" startOffset="0%">
+              - Andre Castillon - Web Developer - UX / UI Designer
+            </textPath>
+          </text>
+        </g>
+      </svg>
+    </div>
   );
 };
 
