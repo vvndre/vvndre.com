26ec9c1d4f046878b1ec469147294a26274c2bf6
andrercastillon <andrercastillon@gmail.com>
2024-04-14 17:09:46 -0500

    favicon head tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/public/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
       </head>
       <body className={`${dots.variable} ${oswald.variable} ${lora.variable}`}>
         <NavBar />
