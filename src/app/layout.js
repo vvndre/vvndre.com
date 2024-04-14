09d6d0ae05f8a232c681dbf0d851445394a5c3d6
andrercastillon <andrercastillon@gmail.com>
2024-04-14 17:31:04 -0500

    (refactor): added icon to metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "vvndre",
   description: "Portfolio for Andre Castillon",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
