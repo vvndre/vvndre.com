300d1cd9ae98bb238edb433f0af091124b11fa1b
andrercastillon <andrercastillon@gmail.com>
2024-04-14 17:36:48 -0500

    (refactor): added head and link tags for icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,14 +9,25 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "vvndre",
   description: "Portfolio for Andre Castillon",
-  icons: {
-    icon: "/icon.png",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={`${dots.variable} ${oswald.variable} ${lora.variable}`}>
         <link rel="icon" href="/images/icon.png" sizes="any" />
         <NavBar />
