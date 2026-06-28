51974af546079d97b9e7dfdbaa85ca5816c717fe
andrercastillon <andrercastillon@gmail.com>
2026-06-28 16:51:49 -0500

    (hotfix): removed extra xmlns prop

--- src/app/page.js
+++ src/app/page.js
@@ -160,7 +160,6 @@ export default function Home() {
                     class="w-5 h-5"
                     viewBox="0 0 256 256"
                     xmlns="http://www.w3.org/2000/svg"
-                    xmlns="http://www.w3.org/1999/xlink"
                     fill="#8e74ff"
                   >
                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
